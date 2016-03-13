9c72938a84b7ae5927f585242f1ffb25f2b48d0f
Ben Davis <ben@thebendavis.net>
2016-03-12 19:24:36 -0500

    Diminish helm-mode minor mode indicator

--- mode-configs/diminish.conf.el
+++ mode-configs/diminish.conf.el
@@ -2,6 +2,7 @@
 
 (eval-after-load "beacon"              '(diminish 'beacon-mode))
 (eval-after-load "git-gutter"          '(diminish 'git-gutter-mode))
+(eval-after-load "helm"                '(diminish 'helm-mode))
 (eval-after-load "paredit"             '(diminish 'paredit-mode))
 (eval-after-load "rainbow-mode"        '(diminish 'rainbow-mode))
 (eval-after-load "undo-tree"           '(diminish 'undo-tree-mode))
